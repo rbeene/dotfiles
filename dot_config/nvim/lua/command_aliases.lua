ab8e8545a47f4e64677a2724966f34ddb09947ae
Robert Beene <robert@ismly.com>
2024-06-25 21:13:57 -0400

    updates

--- dot_config/nvim/lua/command_aliases.lua
+++ dot_config/nvim/lua/command_aliases.lua
@@ -2,8 +2,8 @@ local M = {}
 
 function M.setup_command_alias(input, output)
   vim.cmd(string.format([[
-    cnoreabbrev <expr> %s ((getcmdtype() == ':' and getcmdline() == '%s') ? '%s' : '%s')
-  ]], vim.fn.escape(input, '\\'), input, output, input)
+    cnoreabbrev <expr> %s ((getcmdtype() == ':') && (getcmdline() == '%s')) ? '%s' : '%s'
+  ]], vim.fn.escape(input, '\\'), input, output, input))
 end
 
 function M.setup_aliases()
